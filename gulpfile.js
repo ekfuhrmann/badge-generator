711cb8fa5e7206ab7b83b47fe4fe9bad14dae6b0
Eric Fuhrmann <ekfuhrmann@users.noreply.github.com>
2016-08-22 14:13:02 -0400

    Updated workflow for new projects moving forward.

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,67 @@
-'use strict';
 var gulp = require('gulp');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
+var p = require('gulp-load-plugins')();
 
-gulp.task('sass', function () {
-    var sassOptions = {
-        errLogToConsole: true,
-        outputStyle: 'compressed', // Minifies the CSS
-    };
-    gulp.src('./styles/dev/*.scss')
-        .pipe(sass(sassOptions).on('error', sass.logError))
-        .pipe(sourcemaps.write())
-        .pipe(autoprefixer())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('./styles/dist/'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+var handle = function(err) {
+  console.log(err); this.emit('end');
+}
+
+gulp.task('server', function() {
+  return p.connect.server({
+    root: './public',
+    port: 8000,
+    livereload: true
+  });
 });
 
-gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        },
-    });
+gulp.task('images', function() {
+  return gulp.src('src/images/**/*')
+    .pipe(p.imagemin({progressive: true}))
+    .on('error', p.notify.onError('Error: <%= error.message %>'))
+    .pipe(gulp.dest('public/images'));
 });
 
-gulp.task('watch', ['browserSync', 'sass'], function() {
-        gulp.watch('./styles/dev/**/*.scss', ['sass']);
-        // Reloads the browser whenever HTML or JS files change
-        gulp.watch('./*.html', browserSync.reload);
-        gulp.watch('./js/dev/*.js', browserSync.reload);
+gulp.task('fonts', function() {
+  return gulp.src('src/fonts/**/*')
+    .pipe(gulp.dest('./public/fonts'));
 });
+
+gulp.task('ejs', function() {
+  return gulp.src('src/views/**/*.ejs')
+    .pipe(p.ejs())
+    .on('error', p.notify.onError('Error: <%= error.message %>'))
+    .pipe(p.htmlmin({collapseWhitespace: true, removeComments: true}))
+    .pipe(p.ejs({}, {ext:'.html'}))
+    .pipe(gulp.dest('./public'))
+    .pipe(p.connect.reload());
+});
+
+gulp.task('sass', function() {
+  return gulp.src('src/styles/style.scss')
+    .pipe(p.sass({outputStyle: 'compressed'}))
+    .on('error', p.notify.onError('Error: <%= error.message %>'))
+    .on('error', handle)
+    .pipe(p.autoprefixer())
+    .pipe(p.rename({suffix: '.min'}))
+    .pipe(gulp.dest('./public'))
+    .pipe(p.connect.reload());
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('src/scripts/*.js')
+    .pipe(p.concat('scripts.js'))
+    .pipe(p.uglify())
+    .on('error', p.notify.onError('Error: <%= error.message %>'))
+    .pipe(gulp.dest('./public'))
+    .pipe(p.connect.reload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/images/**/*', ['images']);
+  gulp.watch('src/fonts/**/*', ['fonts']);
+  gulp.watch('src/views/**/*.ejs', ['ejs']);
+  gulp.watch('src/style/**/*.scss', ['sass']);
+  gulp.watch('src/scripts/*.js', ['scripts']);
+});
+
+gulp.task('default', [ 'server', 'fonts', 'images', 'ejs', 'sass', 'scripts', 'watch' ]);
+gulp.task('build', ['fonts', 'images', 'ejs', 'sass', 'scripts']);
