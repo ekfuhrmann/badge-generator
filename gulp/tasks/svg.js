083ca510602d5a7a334990d2120d3ad707c123b3
Eric Fuhrmann <ekfuhrmann@gmail.com>
2018-04-16 11:29:56 -0400

    Standardized project tab size (2 spaces)

--- gulp/tasks/svg.js
+++ gulp/tasks/svg.js
@@ -5,68 +5,68 @@ const $ = plugins();
 const config = require('../config');
 
 gulp.task('svg', () => {
-    return gulp
-        .src('./src/svg/*.svg')
-        .pipe(
-            $.svgmin({
-                plugins: [
-                    {
-                        removeStyleElement: false
-                    },
-                    {
-                        // removeAttrs: {
-                        //   attrs: ['fill', 'stroke', 'fill.*', 'stroke.*']
-                        // }
-                    }
-                ]
-            })
-        )
-        .pipe(gulp.dest(`${config.distFolder}/assets/svg`));
+  return gulp
+    .src('./src/svg/*.svg')
+    .pipe(
+      $.svgmin({
+        plugins: [
+          {
+            removeStyleElement: false
+          },
+          {
+            // removeAttrs: {
+            //   attrs: ['fill', 'stroke', 'fill.*', 'stroke.*']
+            // }
+          }
+        ]
+      })
+    )
+    .pipe(gulp.dest(`${config.distFolder}/assets/svg`));
 });
 
 gulp.task('svg:inline', () => {
-    return gulp
-        .src('./src/svg/inline/*.svg')
-        .pipe(
-            $.svgmin({
-                plugins: [
-                    {
-                        removeStyleElement: true
-                    },
-                    {
-                        removeAttrs: {
-                            attrs: ['fill', 'stroke', 'fill.*!opacity', 'stroke.*']
-                        }
-                    }
-                ]
-            })
-        )
-        .pipe($.svgstore({ inlineSvg: true }))
-        .pipe(gulp.dest('./src/views/layouts/includes'));
+  return gulp
+    .src('./src/svg/inline/*.svg')
+    .pipe(
+      $.svgmin({
+        plugins: [
+          {
+            removeStyleElement: true
+          },
+          {
+            removeAttrs: {
+              attrs: ['fill', 'stroke', 'fill.*!opacity', 'stroke.*']
+            }
+          }
+        ]
+      })
+    )
+    .pipe($.svgstore({ inlineSvg: true }))
+    .pipe(gulp.dest('./src/views/layouts/includes'));
 });
 
 gulp.task('svg:external', () => {
-    return gulp
-        .src('./src/svg/external/*.svg')
-        .pipe(
-            $.svgmin({
-                plugins: [
-                    {
-                        removeStyleElement: true
-                    },
-                    {
-                        removeAttrs: {
-                            attrs: ['fill', 'stroke', 'fill.*', 'stroke.*']
-                        }
-                    }
-                ]
-                // Uncomment below for pretty SVG output
-                // ,js2svg: {
-                //   pretty: true
-                // }
-            })
-        )
-        .pipe(gulp.dest(`${config.distFolder}/assets/svg`));
+  return gulp
+    .src('./src/svg/external/*.svg')
+    .pipe(
+      $.svgmin({
+        plugins: [
+          {
+            removeStyleElement: true
+          },
+          {
+            removeAttrs: {
+              attrs: ['fill', 'stroke', 'fill.*', 'stroke.*']
+            }
+          }
+        ]
+        // Uncomment below for pretty SVG output
+        // ,js2svg: {
+        //   pretty: true
+        // }
+      })
+    )
+    .pipe(gulp.dest(`${config.distFolder}/assets/svg`));
 });
 
 gulp.task('svg', gulp.parallel('svg', 'svg:inline', 'svg:external'));
--- gulpfile.js
+++ gulpfile.js
@@ -7,57 +7,57 @@ const config = require('./gulp/config');
 
 // Let's require all the tasks inside gulp/tasks
 requireDir('./gulp/tasks', {
-    recurse: true
+  recurse: true
 });
 
 // The main building block task
 gulp.task(
-    'build',
-    gulp.series(
-        'clean',
-        'svg',
-        'pug',
-        'favicons',
-        'fonts',
-        'scss',
-        'scripts',
-        'imagemin',
-        'assets'
-    )
+  'build',
+  gulp.series(
+    'clean',
+    'svg',
+    'pug',
+    'favicons',
+    'fonts',
+    'scss',
+    'scripts',
+    'imagemin',
+    'assets'
+  )
 );
 
 // Function to properly reload your browser
 function reload(done) {
-    browserSync.reload();
-    done();
+  browserSync.reload();
+  done();
 }
 
 gulp.task('browser-sync', () => {
-    return browserSync.init({
-        server: {
-            baseDir: config.distFolder,
-            serveStaticOptions: {
-                extensions: ['html']
-            }
-        },
-        port: 8000,
-        open: false,
-        notify: false,
-        logConnections: true
-    });
+  return browserSync.init({
+    server: {
+      baseDir: config.distFolder,
+      serveStaticOptions: {
+        extensions: ['html']
+      }
+    },
+    port: 8000,
+    open: false,
+    notify: false,
+    logConnections: true
+  });
 });
 
 gulp.task('watch', done => {
-    gulp.watch('src/svg/*', gulp.series('svg', reload));
-    gulp.watch('src/svg/inline/**/*', gulp.series('svg:inline', reload));
-    gulp.watch('src/svg/external/**/*', gulp.series('svg:external', reload));
-    gulp.watch('src/styles/**/*.scss', gulp.series('scss', reload));
-    gulp.watch('src/scripts/**/*', gulp.series('scripts', reload));
-    gulp.watch('src/favicons/**/*', gulp.series('favicons', reload));
-    gulp.watch('src/assets/**/*', gulp.series('assets', reload));
-    gulp.watch('src/fonts/**/*', gulp.series('fonts', reload));
-    gulp.watch('src/views/**/*', gulp.series('pug', reload));
-    done();
+  gulp.watch('src/svg/*', gulp.series('svg', reload));
+  gulp.watch('src/svg/inline/**/*', gulp.series('svg:inline', reload));
+  gulp.watch('src/svg/external/**/*', gulp.series('svg:external', reload));
+  gulp.watch('src/styles/**/*.scss', gulp.series('scss', reload));
+  gulp.watch('src/scripts/**/*', gulp.series('scripts', reload));
+  gulp.watch('src/favicons/**/*', gulp.series('favicons', reload));
+  gulp.watch('src/assets/**/*', gulp.series('assets', reload));
+  gulp.watch('src/fonts/**/*', gulp.series('fonts', reload));
+  gulp.watch('src/views/**/*', gulp.series('pug', reload));
+  done();
 });
 
 gulp.task('serve', gulp.parallel('browser-sync', 'watch'));
