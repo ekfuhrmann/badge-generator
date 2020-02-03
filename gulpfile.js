fb75d6f9c1a5072759ee30603977a4da9ac54ed7
ekfuhrmann@gmail.com <ekfuhrmann@gmail.com>
2020-02-03 17:16:39 -0500

    Removed deploy and changed " to '

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync');
-const requireDir = require('require-dir');
+const gulp = require("gulp");
+const browserSync = require("browser-sync");
+const requireDir = require("require-dir");
 // Let's require all the tasks inside gulp/tasks
-requireDir('./gulp/tasks', {
+requireDir("./gulp/tasks", {
   recurse: true
 });
 
 // The main building block task
 gulp.task(
-  'build',
-  gulp.series('clean', 'svg', 'pug', 'styles', 'scripts', 'images', 'assets')
+  "build",
+  gulp.series("clean", "svg", "pug", "styles", "scripts", "images", "assets")
 );
 
 // Function to properly reload your browser
@@ -19,25 +19,25 @@ function reload(done) {
 }
 
 const sendMaps = (req, res, next) => {
-  const filename = req.url.split('/').pop();
-  const extension = filename.split('.').pop();
+  const filename = req.url.split("/").pop();
+  const extension = filename.split(".").pop();
 
-  if (extension === 'js') {
+  if (extension === "js") {
     // res.setHeader('X-SourceMap', '/assets/js/' + filename + '.map');
-  } else if (extension === 'css') {
-    res.setHeader('X-SourceMap', `/assets/css/${filename}.map`);
+  } else if (extension === "css") {
+    res.setHeader("X-SourceMap", `/assets/css/${filename}.map`);
   }
 
   return next();
 };
 
-gulp.task('browser-sync', () =>
+gulp.task("browser-sync", () =>
   browserSync.init({
     server: {
-      baseDir: './dist',
+      baseDir: "./dist",
       middleware: [sendMaps],
       serveStaticOptions: {
-        extensions: ['html']
+        extensions: ["html"]
       }
     },
     port: 8000,
@@ -47,23 +47,20 @@ gulp.task('browser-sync', () =>
   })
 );
 
-gulp.task('watch', done => {
-  gulp.watch('src/svg/inline/**/*', gulp.series('svg:inline', reload));
-  gulp.watch('src/svg/external/**/*', gulp.series('svg:external', reload));
-  gulp.watch('src/sass/**/*.scss', gulp.series('styles', reload));
-  gulp.watch('src/js/**/*', gulp.series('scripts', reload));
+gulp.task("watch", done => {
+  gulp.watch("src/svg/inline/**/*", gulp.series("svg:inline", reload));
+  gulp.watch("src/svg/external/**/*", gulp.series("svg:external", reload));
+  gulp.watch("src/sass/**/*.scss", gulp.series("styles", reload));
+  gulp.watch("src/js/**/*", gulp.series("scripts", reload));
   gulp.watch(
-    ['src/assets/**/*', '!./src/assets/images/**/*'],
-    gulp.series('assets', reload)
+    ["src/assets/**/*", "!./src/assets/images/**/*"],
+    gulp.series("assets", reload)
   );
-  gulp.watch('src/assets/images/**/*', gulp.series('images', reload));
-  gulp.watch('src/views/**/*', gulp.series('pug', reload));
+  gulp.watch("src/assets/images/**/*", gulp.series("images", reload));
+  gulp.watch("src/views/**/*", gulp.series("pug", reload));
   done();
 });
 
-gulp.task('serve', gulp.parallel('browser-sync', 'watch'));
+gulp.task("serve", gulp.parallel("browser-sync", "watch"));
 
-gulp.task('default', gulp.series('build', 'serve'));
-
-// Task for deploying to GhPages
-gulp.task('deploy', gulp.series('clean', 'build', 'deploy:ghPages'));
+gulp.task("default", gulp.series("build", "serve"));
