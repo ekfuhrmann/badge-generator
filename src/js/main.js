af218d962d05b23bf2a1a4ac9960fbdb1d6cffdf
ekfuhrmann <ekfuhrmann@gmail.com>
2021-04-19 19:25:25 -0400

    Ugh

--- src/js/main.js
+++ src/js/main.js
@@ -1,57 +1,130 @@
-import htmlToSvg from 'htmlsvg';
+// import htmlToSvg from 'htmlsvg';
 import opentype from 'opentype.js';
 import { load } from 'opentype.js';
+import computeLayout from 'opentype-layout';
 
 // Reserved for scripts
 const main = () => {
-  // async function make() {
-  //   const font = await opentype.load(
-  //     './assets/fonts/roboto/roboto-medium-webfont.woff'
-  //   );
-  //   const path = font.getPath('FUCK IT', 0, 0, 72);
-  //   console.log(path.toSVG());
-  // }
-  // make();
-
-  // async function doot() {
-  //   const wrapper = document.querySelector('.wrapper');
-  //   const htmlElement = document.querySelector('.badge');
-  //   const svg = await htmlToSvg(htmlElement);
-  //   // console.log(svg);
-
-  //   wrapper.appendChild(svg);
-  // }
-
-  // doot();
-
   // =============-=-=-=-=-==--=-=-=-=-=
   // const text = document.querySelector('.cls-3');
   // const width = text.getBBox().width();
   // console.log(width);
 
-  const inputPrimary = document.querySelector('.doot[data-type="primary"]');
-  const inputSecondary = document.querySelector('.doot[data-type="secondary"]');
+  const input = document.querySelectorAll('.input');
+  const color = document.querySelectorAll('.color');
+  const shadowSVG = document.querySelector('.shadowSVG');
+  const svgText = document.querySelectorAll('.shadowSVG__text');
+  const svgRect = document.querySelectorAll('.shadowSVG__rect');
 
-  inputPrimary.addEventListener('keyup', (e) => {
-    const svgTextPrimary = document.querySelector(
-      '.shadowSVG__text[data-type="primary"]'
+  async function make() {
+    // const doop = await opentype.load(
+    //   './assets/fonts/roboto/roboto-medium-webfont.woff',
+    //   (err, font) => {
+    //     if (err) throw err;
+
+    //     var fontSizePx = 12;
+    //     var text = 'Fuck It';
+    //     var scale = (1 / font.unitsPerEm) * fontSizePx;
+
+    //     // Layout some text - notice everything is in em units!
+    //     var result = computeLayout(font, text, {
+    //       letterSpacing: 3 * font.unitsPerEm, // '2.5em' in font units
+    //     });
+
+    //     // Array of characters after layout
+    //     console.log(result.glyphs);
+
+    //     // Computed height after word-wrap
+    //     console.log(result.height);
+
+    //     return result;
+    //   }
+    // );
+
+    const font = await opentype.load(
+      './assets/fonts/roboto/roboto-medium-webfont.woff'
     );
 
-    setText(svgTextPrimary, e.target.value);
+    const doink = computeLayout(font, 'Fuck It');
+    console.log(doink);
+
+    const path = font.getPath('FUCK IT', 13, 22, 12, { kerning: false });
+
+    console.log(path);
+    console.log(path.toPathData());
+    const test = document.querySelector('.test');
+    test.setAttributeNS(null, 'd', path.toPathData());
+  }
+
+  input[0].addEventListener('keyup', (e) => {
+    setText(svgText[0], e.target.value);
   });
 
-  inputSecondary.addEventListener('keyup', (e) => {
-    const svgTextSecondary = document.querySelector(
-      '.shadowSVG__text[data-type="secondary"]'
-    );
+  input[1].addEventListener('keyup', (e) => {
+    setText(svgText[1], e.target.value);
+  });
 
-    setText(svgTextSecondary, e.target.value);
+  const formBtn = document.querySelector('.btn');
+
+  formBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('sub');
+    make();
   });
 
   function setText(el, value) {
     el.textContent = value.toUpperCase();
+
+    setTimeout(() => {
+      generateSvg();
+    }, 10);
   }
+
+  function generateSvg() {
+    const primaryTextSize = svgText[0].getBBox().width;
+    const secondaryTextSize = svgText[1].getBBox().width;
+    const primaryColor = color[0];
+    const totalSize = 0;
+
+    shadowSVG.setAttributeNS(
+      null,
+      'viewBox',
+      `0 0 ${primaryTextSize + secondaryTextSize + 52} 35`
+    );
+    shadowSVG.setAttributeNS(
+      null,
+      'width',
+      primaryTextSize + secondaryTextSize + 52
+    );
+    svgRect[0].setAttributeNS(
+      null,
+      'width',
+      primaryTextSize ? primaryTextSize + 26 + (secondaryTextSize ? 2 : 0) : 0
+    );
+    svgRect[0].setAttributeNS(null, 'fill', color[0].value);
+    svgRect[1].setAttributeNS(null, 'fill', color[1].value);
+    svgRect[1].setAttributeNS(
+      null,
+      'width',
+      secondaryTextSize ? secondaryTextSize + 26 : 0
+    );
+    svgRect[1].setAttributeNS(null, 'x', primaryTextSize + 26);
+    svgText[1].setAttributeNS(null, 'x', primaryTextSize + 39);
+  }
+
+  const generateSVG = document.querySelector('.generateSVG');
+
+  const tagPrimary = document.querySelector(
+    '.badge__tagline[data-type="primary"]'
+  );
+  const tagSecondary = document.querySelector(
+    '.badge__tagline[data-type="secondary"]'
+  );
 };
+
+class Svg {
+  constructor() {}
+}
 
 document.addEventListener('DOMContentLoaded', main);
 
