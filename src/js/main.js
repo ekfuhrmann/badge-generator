6504a3b7b2307da1d21c9a17bdccb9e34c799833
ekfuhrmann <ekfuhrmann@gmail.com>
2021-04-23 15:27:45 -0400

    Refactored, commented, and cleaned up code

--- src/js/main.js
+++ src/js/main.js
@@ -1,153 +1,29 @@
-// import htmlToSvg from 'htmlsvg';
 import opentype from 'opentype.js';
-import {
-  badge,
-  convertToPath,
-  pathGenerator,
-  textToPath,
-  buildSvg,
-} from './partials/svgPath';
+import badge from './partials/badge';
 
 // Reserved for scripts
 const main = () => {
   const input = document.querySelectorAll('.input');
-  const color = document.querySelectorAll('.color');
-  const shadowSVG = document.querySelector('.shadowSVG');
-  // const svgText = document.querySelectorAll('.shadowSVG__text');
-  const svgRect = document.querySelectorAll('.shadowSVG__rect');
-  const svgText = document.querySelectorAll('.text');
-
-  // async function make(string) {
-  //   const font = await opentype.load(
-  //     './assets/fonts/roboto/roboto-medium-webfont.woff'
-  //   );
-
-  //   const path = font.getPath(string, 13, 22, 12, { kerning: true });
-
-  //   console.log(path);
-
-  //   // test.setAttributeNS(null, 'd', getSvgPath(doink.glyphs[0].data.getPath()));
-  // }
-
-  input[0].addEventListener('keyup', (e) => {
-    // setText(svgText[0], e.target.value);
-
-    //  textToPath({
-    //     text: 'Hello',
-    //     type: 'primary',
-    //   })
-
-    // async function getAll() {
-    //   const boop = await Promise.all([
-    //     textToPath({
-    //       text: 'Hello',
-    //       type: 'primary',
-    //     }),
-    //   ]);
-
-    //   return boop;
-    // }
-
-    // getAll().then((result) => console.log(result));
-
-    // textToPath({
-    //   text: 'Hello',
-    //   type: 'primary',
-    // })
-
-    buildSvg('Test One', 'Test dossada');
-
-    // console.log(doot);
-
-    pathGenerator(e.target.value, 'primary').then((res) => {
-      svgText[0].setAttributeNS(null, 'd', res.doot);
-
-      setTimeout(() => {
-        badge(res.size);
-      }, 10);
-    });
-  });
-
-  input[1].addEventListener('keyup', (e) => {
-    // setText(svgText[1], e.target.value);
-    const text = e.target.value;
-
-    pathGenerator(text, 'secondary').then((res) => {
-      svgText[1].setAttributeNS(null, 'd', res.doot);
-
-      setTimeout(() => {
-        badge(res.size);
-      }, 10);
-    });
-  });
-
   const formBtn = document.querySelector('.btn');
 
-  formBtn.addEventListener('click', (e) => {
-    e.preventDefault();
+  // build svg on load using default values
+  badge(input[0].value, input[1].value);
 
-    // setText(svgText[0], input[0].value);
-    // setText(svgText[1], input[1].value);
-
-    // make(formatText(input[0].value));
-
-    pathGenerator(input[0].value, 'primary').then((res) => {
-      svgText[0].setAttributeNS(null, 'd', res.doot);
-    });
-
-    pathGenerator(input[1].value, 'secondary').then((res) => {
-      svgText[1].setAttributeNS(null, 'd', res.doot);
-    });
+  // build svg while adding primary input values
+  input[0].addEventListener('keyup', (e) => {
+    badge(input[0].value, input[1].value);
   });
 
-  function setText(el, value) {
-    text.toUpperCase().split('').join(' ');
+  // build svg while adding secondary input values
+  input[1].addEventListener('ke9yup', (e) => {
+    badge(input[0].value, input[1].value);
+  });
 
-    setTimeout(() => {
-      generateSvg();
-    }, 10);
-  }
-
-  function generateSvg() {
-    const primaryTextSize = svgText[0].getBBox().width;
-    const secondaryTextSize = svgText[1].getBBox().width;
-
-    console.log(primaryTextSize);
-
-    shadowSVG.setAttributeNS(
-      null,
-      'viewBox',
-      `0 0 ${primaryTextSize + secondaryTextSize + 52} 35`
-    );
-    shadowSVG.setAttributeNS(
-      null,
-      'width',
-      primaryTextSize + secondaryTextSize + 52
-    );
-    svgRect[0].setAttributeNS(
-      null,
-      'width',
-      primaryTextSize ? primaryTextSize + 26 + (secondaryTextSize ? 2 : 0) : 0
-    );
-    svgRect[0].setAttributeNS(null, 'fill', color[0].value);
-    svgRect[1].setAttributeNS(null, 'fill', color[1].value);
-    svgRect[1].setAttributeNS(
-      null,
-      'width',
-      secondaryTextSize ? secondaryTextSize + 26 : 0
-    );
-    svgRect[1].setAttributeNS(null, 'x', primaryTextSize + 26);
-    svgText[1].setAttributeNS(null, 'x', primaryTextSize + 39);
-  }
-
-  const generateSVG = document.querySelector('.generateSVG');
-
-  const tagPrimary = document.querySelector(
-    '.badge__tagline[data-type="primary"]'
-  );
-  const tagSecondary = document.querySelector(
-    '.badge__tagline[data-type="secondary"]'
-  );
+  // build svg when clicking button
+  formBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    badge(input[0].value, input[1].value);
+  });
 };
 
 document.addEventListener('DOMContentLoaded', main);
---
+++ src/js/partials/badge.js
@@ -0,0 +1,161 @@
+import opentype from 'opentype.js';
+
+/**
+ * In order to properly identify each letterform within
+ * a  path so that I can manually apply letter-spacing,
+ * I'm adding a space between each letter value, and then
+ * using pathSpacing() to test each `M` type against the
+ * prior `M` type based on the distance from one another.
+ * This allows me to differentiate interior `M` types in
+ * a letterform such as the interior path in the letter A,
+ * from other `M` types which represent new letterforms.
+ * As such if I know the space from one `M` type to another
+ * is > than a set value, it must be a new letterform, in
+ * which case I want to apply specified letter-spacing.
+ */
+
+// formats text prior to adding letter-spacing
+const formatText = (text) => {
+  return text.toUpperCase().split('').join(' ');
+};
+
+// mutates the path to account for the proper letter-spacing
+const pathSpacing = (data) => {
+  const letterSpacing = -0.35; // reducing spacing slightly due to the added spaces
+  let mCount = -1; // iterates up on each new letterform
+  let mPos; // prior M type position to test against
+
+  return data.map(({ x, y, x1, y1, x2, y2, type }, index) => {
+    switch (type) {
+      case 'Z':
+        return { type };
+      case 'M':
+        if (mPos + 3 < x) {
+          mPos = x;
+          mCount++;
+        }
+        return {
+          type,
+          x: (x || 0) + mCount * letterSpacing,
+          y,
+        };
+      case 'L':
+        return {
+          type,
+          x: (x || 0) + mCount * letterSpacing,
+          y,
+        };
+      case 'Q':
+        return {
+          type,
+          x1: (x1 || 0) + mCount * letterSpacing,
+          y1,
+          x: (x || 0) + mCount * letterSpacing,
+          y,
+        };
+      case 'C':
+        return {
+          type,
+          x1: (x1 || 0) + mCount * letterSpacing,
+          y1,
+          x2: (x2 || 0) + mCount * letterSpacing,
+          y2,
+          x: (x || 0) + mCount * letterSpacing,
+          y,
+        };
+      default:
+        throw new Error('invalid glyph path type: ' + type);
+    }
+  });
+};
+
+// converts text to path and returns the size of the path
+const textToPath = async ({ text, type, offset = 0 }) => {
+  const primaryFont = './assets/fonts/roboto/roboto-medium-webfont.woff';
+  const secondaryFont =
+    './assets/fonts/montserrat/montserrat-extrabold-webfont.woff';
+
+  const font = await opentype.load(
+    type === 'secondary' ? secondaryFont : primaryFont
+  );
+
+  // use opentype.js to convert text to path
+  const path = font.getPath(
+    formatText(text),
+    offset ? offset + 39 : 13,
+    22,
+    12
+  );
+
+  // update path to incorporate letter-spacing
+  path.commands = pathSpacing(path.commands);
+
+  // get path size (width) rounded up 2 decimal places
+  const size =
+    path.getBoundingBox().x2.toFixed(2) - path.getBoundingBox().x1.toFixed(2);
+
+  return {
+    path: path.toPathData(),
+    size,
+  };
+};
+
+const badge = async (textPrimary, textSecondary) => {
+  const svg = document.querySelector('.svg');
+  const svgRect = document.querySelectorAll('.svg__rect');
+  const svgText = document.querySelectorAll('.svg__text');
+  const color = document.querySelectorAll('.color');
+
+  // draw primary text path
+  const primary = await textToPath({
+    text: textPrimary,
+    type: 'primary',
+  });
+
+  // draw secondary text path
+  const secondary = await textToPath({
+    text: textSecondary,
+    type: 'secondary',
+    offset: primary.size,
+  });
+
+  // update svg attributes based on path size + padding
+  svg.setAttributeNS(
+    null,
+    'viewBox',
+    `0 0 ${primary.size + secondary.size + 52} 35`
+  );
+  svg.setAttributeNS(null, 'width', primary.size + secondary.size + 52);
+
+  // update path 'd' attribute
+  svgText[0].setAttributeNS(null, 'd', primary.path);
+  svgText[1].setAttributeNS(null, 'd', secondary.path);
+
+  /**
+   * the following primary rect has a special conditional
+   * in there to add 2px of overflow in the case there is
+   * a secondary path. The reason for this is to account
+   * for subpixel rendering which can cause a white line
+   * to appear at certain sizes. By adding a slight bleed
+   * to the rect, we no longer need to worry about the
+   * subpixel render problem
+   */
+
+  // update rect attributes (color, width, position)
+  svgRect[0].setAttributeNS(
+    null,
+    'width',
+    primary.size ? primary.size + 26 + (secondary.size ? 2 : 0) : 0
+  );
+  svgRect[0].setAttributeNS(null, 'fill', color[0].value);
+  svgRect[1].setAttributeNS(
+    null,
+    'width',
+    secondary.size ? secondary.size + 26 : 0
+  );
+  svgRect[1].setAttributeNS(null, 'fill', color[1].value);
+  svgRect[1].setAttributeNS(null, 'x', primary.size ? primary.size + 26 : 0);
+  svgText[1].setAttributeNS(null, 'x', primary.size + 39);
+};
+
+export default badge;
